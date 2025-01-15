1bf66f27dc5c20bf39433b49537c35f2dbcded11
Stasa Stanisic <stasa.stanisic@symphony.is>
2025-01-15 19:26:11 +0100

    add sample components

---
+++ src/components/ExampleComponent/ExampleComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExampleComponent: React.FC = () => {
+  return <div>Hello, Component Library!</div>;
+};
+
+export default ExampleComponent;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from "./Button";
+export { default as ExampleComponent } from "./ExampleComponent";
+export { default as Input } from "./Input";
+export { default as Smartrating } from "./Smartrating";
