6086ed2ac874c02a5c426f09b92e64323a7a071b
Stasa Stanisic <stasa.stanisic@symphony.is>
2025-01-15 19:35:33 +0100

    wip: config tweaks

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,21 +5,28 @@ import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
 import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default [
   {
-    external: ["react", "react-dom"],
     input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
+        // dir: "dist",
         format: "cjs",
         sourcemap: true,
+        // preserveModules: true,
+        // preserveModulesRoot: "src",
       },
       {
         file: packageJson.module,
+        // dir: "dist",
         format: "esm",
+        exports: "named",
         sourcemap: true,
+        // preserveModules: true,
+        // preserveModulesRoot: "src",
       },
     ],
     plugins: [
@@ -29,14 +36,19 @@ export default [
         tsconfig: "./tsconfig.json",
         exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.ts"],
       }),
-      // terser(),
       postcss({ extensions: [".css"], inject: true, extract: false }),
+      // terser(),
+      // visualizer({
+      //   filename: "bundle-analysis.html",
+      //   open: true,
+      // }),
     ],
+    external: ["react", "react-dom", "react/jsx-runtime"],
   },
   {
     input: "src/index.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
+    external: [/\.css$/, "react", "react-dom"],
   },
 ];
